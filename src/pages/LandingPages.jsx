18b2066a43c4927c48a428cdbf23a865ce5480fb
NAYLA-PPLG1 <naylapplg1@gmail.com>
2025-02-16 14:23:59 +0700

    update Caption

--- src/pages/LandingPages.jsx
+++ src/pages/LandingPages.jsx
@@ -4,10 +4,7 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Link } from 'react-router-dom'
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Todos', href: '/pages/home' },
 ]
 
 const LandingPages = () => {
@@ -116,18 +113,17 @@ const LandingPages = () => {
           
           <div className="text-center">
             <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-              Get Thinks Done!
+              Get Things Done!
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat.
+            Simplify Your Life with an Intuitive Todo List. Plan Your Day, Set Your Goals, and Stay on Track with a Seamless Task Management Experience!
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
                 to="/pages/home"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Get started
+                Go to Page
               </Link>
             </div>
           </div>
